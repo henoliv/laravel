e4c1d96c55cf0731ab359bf40be5be0f5296441d
Hostnet Dev <henrique@hostnet.com.br>
2017-11-10 18:56:00 -0200

    Listagem de produtos

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -2,10 +2,24 @@
 
 namespace Estoque\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+
 class ProdutoController extends Controller
 {
-    public function lista()
+    public function lista(): string
     {
-        return "<h1>Listagem de produtos com Laravel</h1>";
+        # Coletando
+        $produtos = DB::select("SELECT * from produtos");
+
+        $html = "<h1>Listagem de produtos com Laravel</h1>";
+        $html .= "<ul>";
+
+        foreach ($produtos as $produto) {
+            $html .= "<li>Nome: {$produto->nome}, Descrição: {$produto->descricao}</li>";
+        }
+
+        $html .= "</ul>";
+
+        return $html;
     }
 }
