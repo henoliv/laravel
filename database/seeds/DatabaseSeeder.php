fb43b63b68444ec0c70a6b2a0906a820ae88cdac
Henrique Rodrigues <henrique@hostnet.com.br>
2017-11-16 17:40:32 -0200

    Seeds e migrations

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,46 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(ProdutoTableSeeder::class);
+    }
+}
+
+class ProdutoTableSeeder extends Seeder
+{
+    
+    public function run()
+    {
+        DB::insert('INSERT INTO produtos
+            (nome, quantidade, valor, descricao)
+            values (?,?,?,?)',
+            [
+                'Geladeira',
+                2,
+                5900.00,
+                'Side by Side com gelo na porta'
+            ]
+        );
+
+        DB::insert('INSERT INTO produtos
+            (nome, quantidade, valor, descricao)
+            values (?,?,?,?)',
+            [
+                'Fogão',
+                5,
+                950.00,
+                'Painel automático e forno elétrico'
+            ]
+        );
+
+        DB::insert('INSERT INTO produtos
+            (nome, quantidade, valor, descricao)
+            values (?,?,?,?)',
+            [
+                'Microondas',
+                1,
+                1520.00,
+                'Manda SMS quando termina de esquentar'
+            ]
+        );
     }
 }
