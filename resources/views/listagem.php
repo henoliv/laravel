ce85b705bdb62fb001ae2d103f0b9092f2147629
Henrique Rodrigues <henrique@hostnet.com.br>
2017-12-06 17:30:04 -0200

    Adicionando Bootstrap

--- resources/views/listagem.php
+++ resources/views/listagem.php
@@ -1,16 +1,33 @@
 <!Doctype html>
 <html>
-    <body>
-        <h1>Listagem de Produtos</h1>
-        <table>
-            <?php foreach ($produtos as $produto): ?>
-            <tr>
-                <td><?=$produto->nome?></td>
-                <td><?=$produto->valor?></td>
-                <td><?=$produto->descricao?></td>
-                <td><?=$produto->quantidade?></td>
-            </tr>
-            <?php endforeach; ?>
-        </table>
-    </body>
+
+<head>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Fonts -->
+    <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+    <title>Controle de estoque</title>
+</head>
+
+<body>
+    <h1>Listagem de Produtos</h1>
+    <table class="table table-striped table-bordered table-hover">
+        <?php foreach ($produtos as $produto): ?>
+        <tr>
+            <td>
+                <?=$produto->nome?>
+            </td>
+            <td>
+                <?=$produto->valor?>
+            </td>
+            <td>
+                <?=$produto->descricao?>
+            </td>
+            <td>
+                <?=$produto->quantidade?>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+
 </html>
