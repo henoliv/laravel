a5be717dc2f3655d62bb42110467682f0b049f01
Henrique Rodrigues <henrique@hostnet.com.br>
2017-12-07 15:54:10 -0200

    utilizando o blade para template

---
+++ resources/views/listagem.blade.php
@@ -0,0 +1,37 @@
+@extends('principal') @section('conteudo')
+<h1>Listagem de Produtos</h1>
+<table class="table table-striped table-bordered table-hover">
+	<thead>
+		<tr>
+			<th>Nome</th>
+			<th>Valor</th>
+			<th>Descrição</th>
+			<th>Quantidade em estoque</th>
+			<th>Detalhes</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach ($produtos as $produto): ?>
+		<tr>
+			<td>
+				<?=$produto->nome?>
+			</td>
+			<td>
+				<?=$produto->valor?>
+			</td>
+			<td>
+				<?=$produto->descricao?>
+			</td>
+			<td>
+				<?=$produto->quantidade?>
+			</td>
+			<td>
+				<a href="produtos/mostra/<?=$produto->id?>">
+					<span class="glyphicon glyphicon-search"></span>
+				</a>
+			</td>
+		</tr>
+		<?php endforeach; ?>
+	</tbody>
+</table>
+@stop
