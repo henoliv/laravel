1bf4494e8c7cfa02e024ba9a6b36c03ac1c92fb3
Henrique Rodrigues <henrique@hostnet.com.br>
2017-12-07 17:14:10 -0200

    corrigindo link para os detalhes

--- resources/views/listagem.blade.php
+++ resources/views/listagem.blade.php
@@ -1,29 +1,34 @@
-@extends('principal') @section('conteudo')
-<h1>Listagem de Produtos</h1>
-<table class="table table-striped table-bordered table-hover">
-	<thead>
-		<tr>
-			<th>Nome</th>
-			<th>Valor</th>
-			<th>Descrição</th>
-			<th>Quantidade em estoque</th>
-			<th>Detalhes</th>
-		</tr>
-	</thead>
-	<tbody>
-		@foreach ($produtos as $produto)
-		<tr>
-			<td>{{ $produto->nome }}</td>
-			<td>{{ $produto->valor }}</td>
-			<td>{{ $produto->descricao }}</td>
-			<td>{{ $produto->quantidade }}</td>
-			<td>
-				<a href="produtos/mostra/{{ $produto->id }}">
-					<span class="glyphicon glyphicon-search"></span>
-				</a>
-			</td>
-		</tr>
-		@endforeach
-	</tbody>
-</table>
-@stop
+@extends('principal')
+@section('conteudo')
+	@if(empty($produtos))
+		<div> Você não tme nenhum prduto cadastrado </aiv>
+	@else
+		<h1>Listagem de Produtos</h1>
+		<table class="table table-striped table-bordered table-hover">
+			<thead>
+				<tr>
+					<th>Nome</th>
+					<th>Valor</th>
+					<th>Descrição</th>
+					<th>Quantidade em estoque</th>
+					<th>Detalhes</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach ($produtos as $produto)
+				<tr>
+					<td>{{ $produto->nome }}</td>
+					<td>{{ $produto->valor }}</td>
+					<td>{{ $produto->descricao }}</td>
+					<td>{{ $produto->quantidade }}</td>
+					<td>
+						<a href="mostra/{{ $produto->id }}">
+							<span class="glyphicon glyphicon-search"></span>
+						</a>
+					</td>
+				</tr>
+				@endforeach
+			</tbody>
+		</table>
+		@endif
+	@stop
